e20568f7c18e73fc3274fefd7b5987ed52fb8239
ポッチャマ <mercworld@pocha.moe>
2025-03-28 14:46:34 -0400

    feat: set tab to be 4 spaces globally
    Signed-off-by: ポッチャマ <mercworld@pocha.moe>

--- init.lua
+++ init.lua
@@ -161,6 +161,12 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+-- Tab/space options
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
